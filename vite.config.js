080b5ad675ed2eeafa16ed0ca336ce26e74f429d
Romain Ciszewicz <ciszewiczromain@gmail.com>
2022-11-24 20:31:51 +0100

    change fichier viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from "@vitejs/plugin-vue";
+const path = require('path')
 
 export default defineConfig({
+    define: {
+        'process.env': {}
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+    base: './',
     plugins: [
         vue(),
         laravel({
