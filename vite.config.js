d3944e0ed0ce436543ceb38e8add54fd5decf7d7
Romain Ciszewicz <ciszewiczromain@gmail.com>
2022-11-24 20:47:50 +0100

    test vite

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,25 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from "@vitejs/plugin-vue";
-const path = require('path')
 
 export default defineConfig({
-    define: {
-        'process.env': {}
-    },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-        },
-    },
-    base: './',
     plugins: [
-        vue(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
 });
