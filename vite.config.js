5ab892187a03c7347a46eee732135b993c96fa7d
Romain Ciszewicz <ciszewiczromain@gmail.com>
2022-11-25 13:07:28 +0100

    test vite

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,10 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
+        vue(),
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
         }),
     ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
-        },
-    },
 });
