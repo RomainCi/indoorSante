afcd18dbe7c613f3a847248a48a0d86deeb31513
Romain Ciszewicz <ciszewiczromain@gmail.com>
2022-11-25 13:02:23 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,15 +6,15 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-{{--    @vite('resources/css/app.css')--}}
+    @vite('resources/css/app.css')
 </head>
 <body style="margin: 0" >
 <div id="app">
 
 </div>
 
-
+@vite('resources/js/app.js')
 <script src="https://kit.fontawesome.com/40f157f11e.js" crossorigin="anonymous"></script>
 </body>
-@vite('resources/js/app.js')
+
 </html>
--- vite.config.js
+++ vite.config.js
@@ -12,10 +12,6 @@ export default defineConfig({
                 },
             },
         }),
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
     ],
     resolve: {
         alias: {
