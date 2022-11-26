d8256a15bf136cc9e2ebb8954b1ad18ce97a01d0
Romain Ciszewicz <ciszewiczromain@gmail.com>
2022-11-26 01:33:47 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,5 @@ Route::get('/', function () {
 Route::get('{any}', function () {
     return view('welcome');
 })->where('any', '.*');
+
+URL::forceScheme('https');
