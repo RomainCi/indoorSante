400a82812fc01e6144ea5174d7158231f3b5bbdd
Romain Ciszewicz <ciszewiczromain@gmail.com>
2022-11-29 20:01:21 +0100

    modifiViteConfig

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,7 @@ export default defineConfig({
             input: ['resources/js/app.js',"resources/css/app.css"],
             refresh: true,
         }),
-        vue({
-            transformAssetUrls: {
-                base: null,
-                includeAbsolute: false,
-            },
-        }),
+        vue(),
         // vue()
     ],
 });
